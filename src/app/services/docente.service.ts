bc9cbfe9903895dbb8005962fa32c2e12b2bea24
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-06-08 01:00:15 -0500

    ajustes

--- src/app/services/docente.service.ts
+++ src/app/services/docente.service.ts
@@ -4,33 +4,31 @@ import { Observable } from 'rxjs';
 import { Docente } from '../models/docente.model';
 import { environment } from 'src/environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
 
-
 export class DocenteService {
-
+  
   private apiServerURL = environment.apiBaseURL;
 
   constructor(private http:HttpClient) { }
  
 
   listaDocente(filtro:string):Observable<Docente[]> {
-      return this.http.get<Docente[]>(`${this.apiServerURL}/rest/crudDocente/listaDocentePorNombreLike/`+ filtro);
+      return this.http.get<Docente[]>(this.apiServerURL + "/rest/crudDocente/listaDocentePorNombreLike/"+ filtro);
   }  
 
   registraDocente(obj: Docente): Observable<any>{
-      return this.http.post(`${this.apiServerURL}/rest/crudDocente/registraDocente`, obj);
+      return this.http.post(this.apiServerURL + "/rest/crudDocente/registraDocente", obj);
   }
 
   actualizaDocente(obj: Docente): Observable<any>{
-    return this.http.put(`${this.apiServerURL}/rest/crudDocente/actualizaDocente`, obj);
+    return this.http.put(this.apiServerURL + "/rest/crudDocente/actualizaDocente", obj);
   }
 
   eliminaDocente(id: any): Observable<any>{
-    return this.http.delete(`${this.apiServerURL}/rest/crudDocente/eliminaDocente/` + id);
+    return this.http.delete(this.apiServerURL + "/rest/crudDocente/eliminaDocente/" + id);
   }
 
 }
--- src/app/services/ubigeo.service.ts
+++ src/app/services/ubigeo.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs';
 import { Ubigeo } from '../models/ubigeo.model';
 import { environment } from 'src/environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -16,14 +15,14 @@ export class UbigeoService {
   constructor(private http: HttpClient) {}
 
   listarDepartamento(): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiServerURL}/rest/util/listaDepartamentos`);
+    return this.http.get<string[]>(this.apiServerURL + '/rest/util/listaDepartamentos');
   }
 
   listaProvincias(paramDep: any): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiServerURL}/rest/util/listaProvincias/` + paramDep);
+    return this.http.get<string[]>(this.apiServerURL + '/rest/util/listaProvincias/' + paramDep);
   }
 
   listaDistritos(paramDep: any, paramProv: any): Observable<Ubigeo[]> {
-    return this.http.get<Ubigeo[]>(`${this.apiServerURL}/rest/util/listaDistritos/` + paramDep + `/` + paramProv);
+    return this.http.get<Ubigeo[]>(this.apiServerURL + '/rest/util/listaDistritos/' + paramDep + '/' + paramProv);
   }
 }
